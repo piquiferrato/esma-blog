31e14ba6775148296b0a9a5843835dcfc56ed3a5
Your Name <you@example.com>
2018-03-02 23:51:49 -0300

    rewrite home page

--- resources/views/home.php
+++ resources/views/home.php
@@ -5,6 +5,7 @@
      <meta http-equiv="X-UA-Compatible" content="ie=edge">
      <link rel="stylesheet" type="text/css" href="css/home.css">
      <!-- Compiled and minified CSS -->
+     <script type="text/javascript" src="./js/app.js"></script>
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
@@ -12,13 +13,12 @@
      <title>BLOG</title>
    </head>
 
-   <body class="orange lighten-4">
+   <body class="purple lighten-4">
 
      <header>
-       <nav class="orange">
+       <nav class="purple">
            <div class="nav-wrapper">
-             <a href="#" id="logo-fullscreen" class="brand-logo hide-on-med-and-down">Logo</a>
-             <a href="#" id="logo-mobile" class="brand-logo center hide-on-large-only">Logo</a>
+             <a href="#" id="logo" class="brand-logo hide-on-med-and-down">Logo</a>
              <ul id="nav-mobile" class="right hide-on-med-and-down">
                <li><a href="#" id="nav-item-1">Products</a></li>
                <li><a href="#" id="nav-item-2">About</a></li>
@@ -26,18 +26,52 @@
              </ul>
                <!--menu-mobile-->
              <ul class="right hide-on-large-only">
-               <li><a href="#" class="dropdown-button right"><i class="material-icons center">dehaze</i></a></li>
+               <li><a href="#" class="dropdown-button right purple darken-1"><i class="material-icons center">dehaze</i></a></li>
              </ul>
            </div>
          </nav>
      </header>
+<!--slider-->
+
+<div class="slider">
+  <ul class="slides">
+    <li>
+      <img src="./images/img-1.jpg"> <!-- random image -->
+      <div class="caption center-align">
+        <h3>This is our big Tagline!</h3>
+        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+      </div>
+    </li>
+    <li>
+      <img src="./images/img-1.jpg"> <!-- random image -->
+      <div class="caption left-align">
+        <h3>Left Aligned Caption</h3>
+        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+      </div>
+    </li>
+    <li>
+      <img src="./images/img-1.jpg"> <!-- random image -->
+      <div class="caption right-align">
+        <h3>Right Aligned Caption</h3>
+        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+      </div>
+    </li>
+    <li>
+      <img src="https://lorempixel.com/580/250/nature/4"> <!-- random image -->
+      <div class="caption center-align">
+        <h3>This is our big Tagline!</h3>
+        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+      </div>
+    </li>
+  </ul>
+</div>
 
 <!--cards section-->
 <div class="container">
   <div class="row">
 
     <div class="col s12 m4">
-      <div class="card medium hoverable">
+      <div class="card small hoverable grey lighten-5 z-depth-1">
         <div class="card-image">
           <img src='./images/img-1.jpg'>
           <span class="card-title">Card Title</span>
@@ -46,13 +80,13 @@
           <p>Blasid fudfaskjlf dsakpoidfj doasnfdskal fdosanodsia fndsoadifsa nfdsoa fdno</p>
         </div>
         <div class="card-action">
-          <a href="#" class="waves-effect waves-light btn right">VER MAS</a>
+          <a href="#" class="waves-effect waves-light btn right purple darken-1">VER MAS</a>
         </div>
       </div>
     </div>
 
     <div class="col s12 m4">
-      <div class="card medium hoverable">
+      <div class="card small hoverable grey lighten-5 z-depth-1">
         <div class="card-image">
           <img src='./images/img-2.jpg'>
           <span class="card-title">Card Title</span>
@@ -61,13 +95,13 @@
           <p>Blasid fudfaskjlf dsakpoidfj doasnfdskal fdosanodsia fndsoadifsa nfdsoa fdno</p>
         </div>
         <div class="card-action">
-          <a href="#" class="waves-effect waves-light btn right">VER MAS</a>
+          <a href="#" class="waves-effect waves-light btn right purple darken-1">VER MAS</a>
         </div>
       </div>
     </div>
 
     <div class="col s12 m4">
-      <div class="card medium hoverable">
+      <div class="card small hoverable grey lighten-5 z-depth-1">
         <div class="card-image">
           <img src='./images/img-3.jpg'>
           <span class="card-title">Card Title</span>
@@ -76,29 +110,81 @@
           <p>Blasid fudfaskjlf dsakpoidfj doasnfdskal fdosanodsia fndsoadifsa nfdsoa fdno</p>
         </div>
         <div class="card-action">
-          <a href="#" class="waves-effect waves-light btn right">VER MAS</a>
+          <a href="#" class="waves-effect waves-light btn right purple darken-1">VER MAS</a>
         </div>
       </div>
     </div>
 
-
   </div>
 </div>
 
+<!--personnel section-->
+  <div class="container">
+    <div class="row">
 
-<div class="container">
+      <div class="col s12 m6 l6">
+        <div class="valign-wrapper">
+          <div class="card-panel hoverable grey lighten-5 z-depth-1">
+            <div class="col s2">
+              <img src="images/presidente.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+              <span class="black-text">
+                This is a square image. Add the "circle" class to it to make it appear circular.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
 
+      <div class="col s12 m6 l6">
+        <div class="valign-wrapper">
+          <div class="card-panel hoverable grey lighten-5 z-depth-1">
+            <div class="col s10">
+              <span class="black-text">
+                This is a square image. Add the "circle" class to it to make it appear circular.
+              </span>
+            </div>
+            <div class="col s2">
+              <img src="images/presidente.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m6 l6">
+        <div class="valign-wrapper">
+          <div class="card-panel hoverable grey lighten-5 z-depth-1">
+            <div class="col s2">
+              <img src="images/presidente.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+              <span class="black-text">
+                This is a square image. Add the "circle" class to it to make it appear circular.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m6 l6">
+        <div class="valign-wrapper">
+          <div class="card-panel hoverable grey lighten-5 z-depth-1">
+            <div class="col s10">
+              <span class="black-text">
+                This is a square image. Add the "circle" class to it to make it appear circular.
+              </span>
+            </div>
+            <div class="col s2">
+              <img src="images/presidente.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+  </div>
 </div>
-
-
-
-
-
-
-
-
-
-
 
 
 
